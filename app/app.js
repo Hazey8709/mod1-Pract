a2def69c977d071327863339c440149a8d450799
Hazey8709 <jfortin802@gmail.com>
2023-02-27 23:23:33 -0500

    update

--- app/app.js
+++ app/app.js
@@ -5,20 +5,22 @@ const app = express();
 //! middleware
 app.use(express.json());
 
-//! localhost:4000
+//! localhost:4000/
 app.get("/", (req, res) => {
     res.status(200).json({ message: "service is up" });
 });
 
-//! router
+//! Exercise-Router
 app.use("/exercise", router);
 
+//! Error Handling
 app.use((req, res, next) => {
     const error = new Error("not found");
     error.status = 404;
     next(error);
 });
 
+//! Error Handling
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+//! localhost:4000/exercise/
 router.get("/", (req, res, next) => {
     res.status(200).json({
         message: "successful - get",
@@ -16,7 +17,7 @@ router.get("/:id", (req, res, next) => {
     const id = req.params.id;
     res.status(200).json({
         message: "successful - get by ID",
-        id: 35,
+        id: id,
         metadata: {
             hostname: req.hostname,
             method: req.method,
